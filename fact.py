4c873683d53e0b880dd83463448fe21a6c7ac264
Ramya2605 <ganamukularamya@gmail.com>
2019-12-18 16:33:02 +0530

    sravanirk

---
+++ fact.py
@@ -0,0 +1,11 @@
+num = 7
+factorial =1
+if num < 0:
+    print ('string')
+elif num == 0:
+    print('the factorial list')
+else:
+    for i in range (1,num +1):
+        factorial = factorial*i
+    print('factorial of ',num,'is',factorial)
+        
